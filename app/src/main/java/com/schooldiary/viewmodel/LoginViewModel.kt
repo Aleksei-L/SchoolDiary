4cdef1aa49d874ea791ba39ff65e5eabbfea60ab
HerrKarotte <dvp03ne@yandex.ru>
2025-04-02 14:43:43 +0300

    add viewModel from loginFragment, add login interface

---
+++ app/src/main/java/com/schooldiary/viewmodel/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.schooldiary.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    fun login(login: String?, password: String?) {
+        TODO()
+    }
+}
